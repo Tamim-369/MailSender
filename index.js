e3f417a48afbd05e0a5513690f33d53c5d929ca9
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2024-06-24 20:02:53 -0400

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 import express from "express";
 import nodemailer from "nodemailer";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON
 app.use(express.json());
+
+// CORS configuration
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.ALLOWED_ORIGIN, // Netlify domain
     methods: ["GET", "POST"],
   })
 );
+
+// Additional CORS headers
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.ALLOWED_ORIGIN);
+  res.header("Access-Control-Allow-Methods", "GET,POST");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.post("/api/sendmail", (req, res) => {
   const { name, from, message } = req.body;
 
@@ -21,8 +36,8 @@ app.post("/api/sendmail", (req, res) => {
   }
 
   const mailOptions = {
-    from: "ashiqurrahmantamim369@gmail.com", // sender address
-    to: "ashiqurrahmantamim369@gmail.com", // list of receivers
+    from: process.env.EMAIL_USER, // sender address
+    to: process.env.EMAIL_USER, // list of receivers
     subject: `${name} sent a message from ${from}`, // Subject line
     html: `<pre>${message}</pre>`, // html body
   };
@@ -30,14 +45,15 @@ app.post("/api/sendmail", (req, res) => {
   const transporter = nodemailer.createTransport({
     service: "Gmail", // e.g., 'Gmail', 'Yahoo', 'Outlook'
     auth: {
-      user: "ashiqurrahmantamim369@gmail.com", // your email
-      pass: "lirf mmzf xgvm dzlz", // your email password or app-specific password
+      user: process.env.EMAIL_USER, // your email
+      pass: process.env.EMAIL_PASS, // your email password or app-specific password
     },
   });
 
   transporter.verify((error, success) => {
     if (error) {
-      console.log(error);
+      console.error("Error verifying email configuration", error);
+      return res.status(500).send("Error verifying email configuration");
     } else {
       console.log("Server is ready to take our messages");
     }
@@ -45,7 +61,7 @@ app.post("/api/sendmail", (req, res) => {
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.log(error);
+      console.error("Error sending email", error);
       return res.status(500).send("Error sending email");
     }
     console.log("Message sent: " + info.response);
